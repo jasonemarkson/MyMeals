b5c1a349dc59b285a7e04652650b3ae78f8e859e
jasonemarkson <jasonmarkson76@gmail.com>
2021-02-10 22:02:29 -0500

    built out some Reviews CRUD

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,27 @@
 class RecipesController < ApplicationController
+    def index
+        @recipes = Recipe.all
+    end
+    
+    def new
+        @recipe = Recipe.new
+    end
+
+    def create
+        @recipe = current_user.recipes.build(recipe_params)
+        binding.pry
+        if @recipe.save
+            redirect_to recipes_path
+        else
+            flash[:message] = "Recipe was not added. Please make sure all of the fields are entered"
+            render :new 
+        end
+
+    end
+
+    private
+
+    def recipe_params
+        params.require(:recipe).permit(:name, :ingredients, :instructions)
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_07_190314) do
+ActiveRecord::Schema.define(version: 2021_02_11_025005) do
 
   create_table "recipes", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2021_02_07_190314) do
     t.text "instructions"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "reviews", force: :cascade do |t|
