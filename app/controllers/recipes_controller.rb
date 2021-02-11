38d824ec60ca119662e5a038a1c2769cc7a589d2
jasonemarkson <jasonmarkson76@gmail.com>
2021-02-11 01:05:06 -0500

    added majority of Recipe functionality. Need to put more checks in place to ensure users aren't submitting blank fields

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,14 +9,43 @@ class RecipesController < ApplicationController
 
     def create
         @recipe = current_user.recipes.build(recipe_params)
-        binding.pry
+
         if @recipe.save
-            redirect_to recipes_path
+
+            if !params[:recipe][:name] || !params[:recipe][:ingredients] || !params[:recipe][:instructions]
+                redirect_to recipe_path(@recipe.id)
+            end
+
         else
             flash[:message] = "Recipe was not added. Please make sure all of the fields are entered"
             render :new 
         end
+    end
 
+    def show
+        @recipe = Recipe.find(params[:id])
+    end
+
+    def edit
+        @recipe = Recipe.find(params[:id])
+    end
+
+    def update
+        @recipe = Recipe.find(params[:id])
+
+        if @recipe
+            @recipe.update(recipe_params)
+            redirect_to recipe_path(@recipe)
+        else
+            flash[:message] = "Recipe was not updated. Please make sure all of the fields are entered"
+            render :edit 
+        end
+    end
+
+    def destroy
+        @recipe = Recipe.find(params[:id])
+        @recipe.delete
+        redirect_to recipes_path
     end
 
     private
@@ -24,4 +53,5 @@ class RecipesController < ApplicationController
     def recipe_params
         params.require(:recipe).permit(:name, :ingredients, :instructions)
     end
+
 end
